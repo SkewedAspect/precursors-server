d30190434403fc66d352354e172f0cbee4569926
Christopher S. Case <chris.case@g33xnexus.com>
2011-11-03 23:10:44 -0500

    More work on fleshing things out.

--- tests/client/client.py
+++ tests/client/client.py
@@ -13,7 +13,9 @@ class Client(object):
         pass
 
     def connect(self, host, port):
+        """Connects to the server, and creates our SSL control channel.
 
+        """
         control = Channel('Control')
         if control.connect(host, port, True, 'SSL'):
 
@@ -27,22 +29,64 @@ class Client(object):
 
         return False
 
-class Channel(object):
-    def __init__(self, name, host=None, port=None, reliable=False):
-        self.encryption = None
-        self.name = name
-        self.host = host
-        self.port = port
-        self.reliable = False
+    def sendRequest(self, request):
+        """Sends the request over SSL.
 
-    def connect(self, host, port, reliable=False, encryption=None):
+        """
+        pass
+
+    def sendEvent(self, event):
+        """Sends the event over SSL.
+
+        """
+        pass
+
+    def removeChannel(self, name):
+        try:
+            del self.channels[name]
+
+        except KeyError:
+            pass
+
+class Channel(object):
+    def __init__(self, name):
+        self.name = name
+        self.host = None
+        self.port = None
+        self.key = None
+        self.reliable = None
+        self.encryption = None
+
+    def connect(self, host, port, reliable=False, encryption=None, key=None):
 
         # TODO: Actually connect to something
 
         # Successfully connected. Now set variables:
         self.encryption = encryption
         self.reliable = reliable
+        self.key = key
         self.host = host
         self.port = port
 
         return True
+
+    def connectSSL(self, host, port):
+        pass
+
+    def connectTCP(self, host, port, key=None):
+        pass
+
+    def connectUDP(self, host, port, key=None):
+        pass
+
+    def sendRequest(self, request):
+        """Send the request over the channel.
+
+        """
+        pass
+
+    def sendEvent(self, event):
+        """Send the event over the channel.
+
+        """
+        pass
