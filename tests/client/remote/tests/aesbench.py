f397480a62176143aff1cfe46b25e39e1017988d
David H. Bronke <david.bronke@skewedaspect.com>
2011-11-08 23:42:22 -0600

    Updated sys.path augmentation so it works regardless of where you run it from, and added some better output and a bit of configuration.

--- tests/client/remote/tests/aesbench.py
+++ tests/client/remote/tests/aesbench.py
@@ -1,26 +1,39 @@
 #!/usr/bin/env python
 
 import sys
+import os
 
-sys.path += ['..']
 
-import os, datetime
-from aescrypt import encrypt, decrypt
+# Add the parent directory of the tests dir to the path.
+sys.path += [os.path.dirname(os.path.dirname(os.path.abspath(sys.argv[0])))]
+
+
+import os
+import datetime
+from aescrypt import encrypt
+
+
+plaintextBytes = 150000  # 150K bytes of plaintext
+iterations = 10000
 
 key = os.urandom(16)
 iv = os.urandom(16)
-text = os.urandom(150000) # 150K bytes of plaintext
+text = os.urandom(plaintextBytes)
+
 
 totalTime = datetime.timedelta()
 
 print 'Starting benchmark...'
 
-for i in range(0, 10000):
+for i in range(iterations):
     starttime = datetime.datetime.now()
     text = encrypt(text, key, iv)
     endtime = datetime.datetime.now()
 
     totalTime += endtime - starttime
 
-print 'Average time:', totalTime / 10000
-
+print 'Total time:', totalTime
+print 'Average time: %0.6f ms over %d iterations.' % (
+        totalTime.total_seconds() * 1000 / iterations,
+        iterations
+        )
