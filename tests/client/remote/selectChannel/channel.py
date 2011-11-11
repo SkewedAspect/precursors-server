3da2a05d7373db85e059a0ec1aa03a1676a26ab2
David H. Bronke <whitelynx@gmail.com>
2011-11-11 05:29:32 -0600

    Added an __init__ to SelectChannel which asserts that things are set up correctly, added the 'socket' property and the 'target' field, added some NotImplementedError raises, and did lots of reformatting.

--- tests/client/remote/selectChannel/channel.py
+++ tests/client/remote/selectChannel/channel.py
@@ -3,23 +3,45 @@ import logging
 from remote.channel import Channel
 
 
-logger = logging.getLogger("remote.channel")
+logger = logging.getLogger("remote.selectChannel.channel")
 
 
 class SelectChannel(Channel):
+    def __init__(self, *args, **kwargs):
+        if not hasattr(self, 'target'):
+            self.target = None
+
+        assert(hasattr(self, 'protocolName'))
+
+        # Ensure this is the right type of socket.
+        assert(kwargs['reliable'] == self.reliable)
+        assert(kwargs['ordered'] == self.ordered)
+
+        super(SelectChannel, self).__init__(*args, **kwargs)
+
     def connect(self, remoteHost, remotePort, **kwargs):
         """Connect to the remote host, possibly using encryption.
 
-        remoteHost - The remote host to connect to.
-        remotePort - The port on the remote host to connect to.
+        remoteHost: The remote host to connect to.
+        remotePort: The port on the remote host to connect to.
 
-        Additional keyword arguments are passed on to Channel.connect; see its
-        documentation for more options.
+        Additional keyword arguments are passed on to Channel.connect; see its documentation for more options.
 
         """
-        # Successfully connected. Now call super to set variables and enable
-        # encryption if requested.
+        self.logger.debug("Successfully connected to %s:%d using %s.", remoteHost, remotePort, self.protocolName)
+
+        # Now call super to set variables and enable encryption if requested.
         super(SelectChannel, self).connect(remoteHost, remotePort, **kwargs)
+
+    def get_socket(self):
+        return self._socket
+
+    def set_socket(self, sock):
+        self._socket = sock
+        self.target = sock
+        self.fileno = sock.fileno
+
+    socket = property(get_socket, set_socket, doc="The underlying socket object for this Channel.")
 
     def sendRequest(self, request):
         """Send an request over the channel.
@@ -37,16 +59,17 @@ class SelectChannel(Channel):
         """Receive a request over the channel.
 
         """
-        pass
+        raise NotImplementedError("receiveRequest not yet implemented!")
 
     def receiveEvent(self, event):
         """Receive an event over the channel.
 
         """
-        pass
+        raise NotImplementedError("receiveEvent not yet implemented!")
 
     def _send(self, data):
         """Sends data over the channel, encrypting if required.
 
         """
-        logger.warn("Unimplemented in base class!")
+        self.logger.debug("Sending data over %s.", self.protocolName)
+        self.target.write(data)
