9efed927ef78d8e300a43fbf7f5309b8a95955c1
Christopher S. Case <chris.case@g33xnexus.com>
2011-11-13 13:40:57 -0600

    Turned Cryptor into a stream class.

--- tests/client/remote/cryptor.py
+++ tests/client/remote/cryptor.py
@@ -1,11 +1,12 @@
 from abc import ABCMeta, abstractmethod
+from stream import Stream
 import logging
 
 
 logger = logging.getLogger("remote.crypt")
 
 
-class Cryptor(object):
+class Cryptor(Stream):
     """Abstract base class for cryptographic objects capable
     of performing encryption and decryption of strings.
 
@@ -25,3 +26,19 @@ class Cryptor(object):
 
         """
         pass
+
+    def read(self, requestedBytes=-1):
+        """Read incoming data from the target stream and decrypt it.
+
+        """
+        ciphertext = super(Cryptor, self).read(requestedBytes)
+
+        return self.decrypt(ciphertext)
+
+    def write(self, data):
+        """Encrypt outgoing data and then write it to the target stream.
+
+        """
+        data = self.encrypt(data)
+        return super(Cryptor, self).write(data)
+
