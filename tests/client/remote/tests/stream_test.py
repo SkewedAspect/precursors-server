b323caa9436ee52d1c3a336822428f75c18c54c8
Christopher S. Case <chris.case@g33xnexus.com>
2011-11-13 17:31:00 -0600

    Added Stream unit tests. The queued tests are not completed yet.

---
+++ tests/client/remote/tests/stream_test.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import sys
+import os
+from StringIO import StringIO
+
+# Add the parent directory of the tests dir to the path.
+sys.path += [os.path.dirname(os.path.dirname(os.path.abspath(sys.argv[0])))]
+
+# Get unittest from our wrapper that imports the right version.
+from unittestwrapper import unittest
+
+from stream import Stream
+
+
+class TestStream(unittest.TestCase):
+    def setUp(self):
+        self.text = "This is my text. Isn't it beautiful?"
+
+    def test_stream_read(self):
+        target = StringIO(self.text)
+
+        text = Stream(target).read()
+
+        self.assertEqual(text, self.text)
+
+    def test_stream_write(self):
+        target = StringIO()
+
+        stream = Stream(target)
+        stream.write(self.text)
+
+        self.assertEqual(target.getvalue(), self.text)
+
+    def test_OutgoingQueuedStream_write(self):
+        pass
+
+    def test_IncomingQueuedStream_read(self):
+        pass
+
+    def test_IOQueuedStream_roundtrip(self):
+        pass
+
+
+if __name__ == '__main__':
+    # Build the suite of tests
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestStream)
+
+    # Run the tests
+    unittest.TextTestRunner(verbosity=2).run(suite)
