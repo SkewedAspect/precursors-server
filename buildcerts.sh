600576346a793dba6edffd857dc77afee6d44ba6
Micah Warren <micahw@lordnull.com>
2011-11-15 21:29:40 -0600

    Putting certs and keys in priv automatically.

--- buildcerts.sh
+++ buildcerts.sh
@@ -1,6 +1,6 @@
 if [ ! -f "key" ]; then
 	echo "RSA key does not exist, generating..."
-	ssh-keygen -t rsa -f key -N ""
+	ssh-keygen -t rsa -f priv/key -N ""
 	RES=$?
 	if [ $RES != 0 ]; then
 		echo "Key generation failed with error $RES!"
@@ -10,7 +10,7 @@ fi
 	
 if [ ! -f "precursors.csr" ]; then
 	echo "Certificate Signing Request does not exist, generating..."
-	openssl req -new -key key -out precursors.csr
+	openssl req -new -key priv/key -out priv/precursors.csr
 	RES=$?
 	if [ $RES != 0 ]; then
 		echo "CSR generation failed with error $RES"
@@ -20,7 +20,7 @@ fi
 
 if [ ! -f "precursors.crt" ]; then
 	echo "Certificate does not exists, generating self-signed for a year..."
-	openssl x509 -req -days 365 -in precursors.csr -signkey key -out precursors.crt
+	openssl x509 -req -days 365 -in priv/precursors.csr -signkey priv/key -out priv/precursors.crt
 	RES=$?
 	if [ $RES != 0 ]; then
 		echo "Certificate generation failed with error $RES"
