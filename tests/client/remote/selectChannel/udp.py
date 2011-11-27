4569b331bdb0b7cc0bbb7be688c394d12fc36a81
David H. Bronke <david.bronke@skewedaspect.com>
2011-11-26 20:11:19 -0600

    Updated UDPChannel to use bind instead of connect, to store the default target address, and to actually send and receive packets correctly.

--- tests/client/remote/selectChannel/udp.py
+++ tests/client/remote/selectChannel/udp.py
@@ -14,6 +14,8 @@ class UDPChannel(SelectChannel):
 
         self.protocolName = "UDP"
 
+        self.defaultTargetAddress = None
+
     @classmethod
     def supportsArgs(cls, **kwargs):
         return kwargs['reliable'] == False and kwargs['ordered'] == False
@@ -21,17 +23,30 @@ class UDPChannel(SelectChannel):
     def connect(self, remoteHost, remotePort, **kwargs):
         self.logger.debug("Connecting using UDP.")
 
-        # Connect.
-        self.socket.connect((remoteHost, remotePort))
+        self.defaultTargetAddress = (remoteHost, remotePort)
+
+        # Bind to the local address and port.
+        self.socket.bind()
 
         # Successfully connected. Now call super to set variables and enable
         # encryption if requested.
         super(UDPChannel, self).connect(remoteHost, remotePort, **kwargs)
 
     def _readStream(self, requestedBytes=-1, **kwargs):
+        # We don't support any keyword arguments.
         assert(len(kwargs) == 0)
-        self.socket.recvfrom(requestedBytes)
+
+        (data, address) = self.socket.recvfrom(requestedBytes)
+
+        metadata = {
+                'remoteAddress': address,
+                }
+        return data, metadata
 
     def _writeStream(self, message, **kwargs):
         remoteAddress = kwargs.pop('address', self.defaultTargetAddress)
+
+        # We don't support any other keyword arguments.
         assert(len(kwargs) == 0)
+
+        self.socket.sendto(message, remoteAddress)
