ee6831e7e9cd5cc2846862332a715c406e39ab26
David H. Bronke <whitelynx@gmail.com>
2012-02-15 01:43:23 -0600

    Added pre_client_hooks, with the client login hook that tries to tell the client what level to load.

---
+++ src/pre_client_hooks.erl
@@ -0,0 +1,27 @@
+-module(pre_client_hooks).
+
+-include("log.hrl").
+-include("pre_client.hrl").
+
+-export([register_hooks/0, client_login_hook/2]).
+
+%% ------------------------------------------------------------------
+%% API Function Definitions
+%% ------------------------------------------------------------------
+
+register_hooks() ->
+	?debug("Registering client hooks."),
+	pre_hooks:add_hook(client_logged_in, ?MODULE, client_login_hook, undefined, [node()]).
+
+%% ------------------------------------------------------------------
+
+client_login_hook(undefined, ClientRecord) ->
+	?debug("Client logged in: ~p", [ClientRecord]),
+	#client_info{connection = Connection} = ClientRecord,
+	LevelUrl = <<"scenes/cnk.json">>,
+	LoadLevel = {struct, [
+		{type, <<"setZone">>},
+		{level, LevelUrl}
+	]},
+	pre_client_connection:send(Connection, tcp, event, level, LoadLevel),
+	{ok, undefined}.
