f8ee7bbe6b019565ac86c3aa03c48c80554fb3b8
David H. Bronke <whitelynx@gmail.com>
2012-02-15 01:42:39 -0600

    Updated pre_channel_level to be a full channel, only to realize it's redundant. Now useful only as an example of a channel.

--- src/pre_channel_level.erl
+++ src/pre_channel_level.erl
@@ -1,4 +1,5 @@
-%% @doc Dictates the background content loaded by the client, and dispatches events for entities in a given zone. (?)
+%%% @doc Dictates the background content loaded by the client, and dispatches events for entities in a given zone. (?)
+%%% FIXME: This module should not actually be used! It's a useful example of how to build a channel, though.
 
 -module(pre_channel_level).
 -behavior(gen_server).
@@ -6,41 +7,106 @@
 -include("log.hrl").
 
 % gen_server
--export([start_link/0]).
--export([init/1, handle_call/3, handle_cast/2]).
+-export([start_link/1]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 % pre_client_channels
--export([init/1, client_request/3, client_response/4, client_event/3]).
+-export([client_request/3, client_response/4, client_event/3]).
+
+-record(state, {
+		supervisor_pid :: pid(),
+		worker_pids :: [pid()],
+		client_count = 0 :: non_neg_integer()
+	}).
 
 %% -------------------------------------------------------------------
 %% api
 %% -------------------------------------------------------------------
 
-start_link() ->
-	gen_server:start_link({local, ch3}, ch3, [{wtf_should_go_here, wtf_should_go_here, wtf_should_go_here}], []).
+start_link(Options) ->
+	gen_server:start_link({local, ?MODULE}, ?MODULE, Options, []).
+
+%% -------------------------------------------------------------------
+
+client_request(Client, Request, Pid) ->
+	gen_server:cast(Pid, {client_request, Client, Request}),
+	ok.
+
+client_response(_Client, _Id, _Response, _Pid) ->
+	%gen_server:cast(Pid, {client_response, Client, Id, Response}),
+	ok.
+
+client_event(_Client, _Event, _Pid) ->
+	%gen_server:cast(Pid, {client_event, Client, Event}),
+	ok.
+
+%% -------------------------------------------------------------------
+
+client_connect_hook(undefined, _ClientPid) ->
+	gen_server:cast(?MODULE, client_connected).
+
+client_disconnect_hook(undefined, _ClientPid) ->
+	gen_server:cast(?MODULE, client_disconnected).
 
 %% -------------------------------------------------------------------
 %% gen_server
 %% -------------------------------------------------------------------
 
-init({_ChannelManager, _Module, _Info} = State) ->
+init(supervisor_start) ->
+	ChildSpec = {
+		?MODULE,
+		{pre_channel_level_worker, start_link, []},
+        transient,
+		brutal_kill,
+		worker,
+		[pre_channel_level_worker]
+	},
+	{ok, {{simple_one_for_one, 2, 2}, [ChildSpec]}};
+
+init(Options) ->
+	InitialWorkers = proplists:get_value(workers, Options, 1),
+	{ok, Supervisor} = supervisor:start_link({local, pre_channel_level_sup}, ?MODULE, supervisor_start),
+	WorkerPids = [begin
+		{ok, Pid} = supervisor:start_child(Supervisor, []),
+		Pid
+	end || _ <- lists:seq(1, InitialWorkers)],
+	pre_hooks:add_hook(client_connected, ?MODULE, client_connect_hook, undefined, [node()]),
+	pre_hooks:add_hook(client_disconnected, ?MODULE, client_disconnect_hook, undefined, [node()]),
+	State = #state{supervisor_pid = Supervisor, worker_pids = WorkerPids},
 	{ok, State}.
 
 %% -------------------------------------------------------------------
 
-client_request(Client, Request, Info) ->
-	{ok, Info}.
-
-client_response(Client, Id, Response, Info) ->
-	{ok, Info}.
-
-client_event(Client, Event, Info) ->
-	{ok, Info}.
+handle_call(_, _From, State) ->
+    {reply, invalid, State}.
 
 %% -------------------------------------------------------------------
 
-%TODO: WTF do we do with these? Should these be getting casts and calls from client_*?
-handle_call(request, _From, Chs) ->
-    {reply, something, Chs}.
+handle_cast({client_request, _Client, _Request} = Message, State) ->
+	[FirstWorker | OtherWorkers] = State#state.worker_pids,
+	gen_server:cast(FirstWorker, Message),
+	{noreply, State#state{worker_pids = OtherWorkers ++ [FirstWorker]}};
 
-handle_cast({event, _Ch}, Chs) ->
-    {noreply, Chs}.
+handle_cast(client_connected, State) ->
+	ClientCount = State#state.client_count + 1,
+    {noreply, State#state{client_count = ClientCount}};
+
+handle_cast(client_disconnected, State) ->
+	ClientCount = State#state.client_count - 1,
+    {noreply, State#state{client_count = ClientCount}};
+
+handle_cast(_, State) ->
+    {noreply, State}.
+
+%% -------------------------------------------------------------------
+
+handle_info(_, State) ->
+    {noreply, State}.
+
+%% -------------------------------------------------------------------
+
+terminate(Reason, _State) ->
+	?info("Terminating due to ~p.", [Reason]),
+	ok.
+
+code_change(_OldVersion, State, _Extra) ->
+	{reply, State}.
