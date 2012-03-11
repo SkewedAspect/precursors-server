9afacea34c6766041b502440effd07990f820acb
Micah Warren <micahw@lordnull.com>
2012-03-10 20:00:19 -0600

    Added documentation.

--- src/pre_channel_entity.erl
+++ src/pre_channel_entity.erl
@@ -1,5 +1,8 @@
-%%% @doc Dictates the background content loaded by the client, and dispatches events for entities in a given zone. (?)
-%%% FIXME: This module should not actually be used! It's a useful example of how to build a channel, though.
+%% @doc Dictates the background content loaded by the client, and
+%% dispatches events for entities in a given zone. (?)
+
+% FIXME: This module should not actually be used! It's a useful example of
+% how to build a channel, though.
 
 -module(pre_channel_entity).
 -behavior(gen_server).
@@ -8,7 +11,8 @@
 
 % gen_server
 -export([start_link/1]).
--export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
+	code_change/3]).
 % pre_client_channels
 -export([client_request/4, client_response/4, client_event/3]).
 
@@ -22,28 +26,39 @@
 %% api
 %% -------------------------------------------------------------------
 
+-type(workers_option() :: {'workers', non_neg_integer()}).
+-type(start_option() :: workers_option()).
+-type(start_options() :: [start_option()] | 'supervisor_start').
+
+%% @doc Starts the entity channel master server.
+-spec(start_link/1 :: (Options :: start_options()) -> {'ok', pid()}).
 start_link(Options) ->
 	gen_server:start_link({local, ?MODULE}, ?MODULE, Options, []).
 
 %% -------------------------------------------------------------------
 
+%% @hidden
 client_request(Client, Id, Request, Pid) ->
 	gen_server:cast(Pid, {client_request, Client, Id, Request}),
 	ok.
 
+%% @hidden
 client_response(_Client, _Id, _Response, _Pid) ->
 	%gen_server:cast(Pid, {client_response, Client, Id, Response}),
 	ok.
 
+%% @hidden
 client_event(_Client, _Event, _Pid) ->
 	%gen_server:cast(Pid, {client_event, Client, Event}),
 	ok.
 
 %% -------------------------------------------------------------------
 
+%% @doc Connect to a given client.
 client_connect_hook(undefined, _ClientPid) ->
 	gen_server:cast(?MODULE, client_connected).
 
+%% @doc Disconnect from a given client.
 client_disconnect_hook(undefined, _ClientPid) ->
 	gen_server:cast(?MODULE, client_disconnected).
 
@@ -51,6 +66,7 @@ client_disconnect_hook(undefined, _ClientPid) ->
 %% gen_server
 %% -------------------------------------------------------------------
 
+%% @hidden
 init(supervisor_start) ->
 	ChildSpec = {
 		?MODULE,
@@ -76,11 +92,13 @@ init(Options) ->
 
 %% -------------------------------------------------------------------
 
+%% @hidden
 handle_call(_, _From, State) ->
     {reply, invalid, State}.
 
 %% -------------------------------------------------------------------
 
+%% @hidden
 handle_cast({client_request, _Client, _Id, _Request} = Message, State) ->
 	[FirstWorker | OtherWorkers] = State#state.worker_pids,
 	gen_server:cast(FirstWorker, Message),
@@ -99,14 +117,17 @@ handle_cast(_, State) ->
 
 %% -------------------------------------------------------------------
 
+%% @hidden
 handle_info(_, State) ->
     {noreply, State}.
 
 %% -------------------------------------------------------------------
 
+%% @hidden
 terminate(Reason, _State) ->
 	?info("Terminating due to ~p.", [Reason]),
 	ok.
 
+%% @hidden
 code_change(_OldVersion, State, _Extra) ->
 	{reply, State}.
