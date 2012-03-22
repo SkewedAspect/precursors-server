ac19923afaf27dfb56673bd6d325cb2aab572e80
Christopher S. Case <chris.case@g33xnexus.com>
2012-03-22 17:42:57 -0500

    Correctly get authentication information from mongo. Correctly accepts and denies users. Ahem.... IT WORKS!!!!!!!

--- included_apps/mongo_auth/src/mongo_auth.erl
+++ included_apps/mongo_auth/src/mongo_auth.erl
@@ -22,7 +22,10 @@
 -include_lib("precursors_server/include/log.hrl").
 -include_lib("precursors_server/include/internal_auth.hrl").
 
--record(state, {}).
+-record(state, {
+			host,
+			conn_pool
+		}).
 
 %% ----------------------------------------------------------------------------
 %% External API
@@ -47,21 +50,34 @@ get_user(Username, Pid) ->
 %% ----------------------------------------------------------------------------
 
 init({gen_server, Args}) ->
-	{ok, Args};
+	
+	%TODO: Get this from our configuration
+	% Connect to mongodb
+	Host = {localhost, 27017},
+	Pool = resource_pool:new (mongo:connect_factory (Host), 10),
+	State = #state{
+		host = Host,
+		conn_pool = Pool
+	},
+	{ok, State};
+
 
 init(Args) ->
 	?debug("Starting MongoDB Auth Plugin"),
 	ok = start_app(mongo_auth),
+	% Start our gen_server
 	Pid = mongo_auth_sup:start_server(Args),
 	{ok, Pid}.
 
 % -----------------------------------------------------------------------------
 
-handle_call({user, _Username}, _From, State) ->
-	{reply, undefined, State};
+handle_call({user, Username}, _From, State) ->
+	Info = handle_userinfo(Username, State),
+	{reply, Info, State};
 
-handle_call({auth, {_Username, _Password}}, _From, State) ->
-	{reply, {deny, "Reasons not yet implemented."}, State};
+handle_call({auth, {Username, Password}}, _From, State) ->
+	Auth = handle_auth(Username, Password, State),
+	{reply, Auth, State};
 
 handle_call(_, _From, State) ->
     {reply, invalid, State}.
@@ -89,6 +105,41 @@ code_change(_OldVersion, State, _Extra) ->
 %% Internal API
 %% ----------------------------------------------------------------------------
 
+%% @doc Authenticate user against mongo
+handle_auth(Username, Password, State) ->
+	case get_account_info(Username, State) of
+		{error, Reason} ->
+			{deny, Reason};
+
+		{Account} ->
+			?info("Got Account Record: ~p", [Account]),
+			PasswordHash = bson:at(password_hash, Account),
+			PasswordSalt = bson:at(password_salt, Account),
+			AuthPasswordHash = string:to_lower(generate_hash(Password, PasswordSalt)),
+			PassHashString = binary_to_list(PasswordHash),
+			?info("Got db: ~p, hash: ~p", [PassHashString, AuthPasswordHash]),
+			case PassHashString of
+				AuthPasswordHash ->
+					allow;
+
+				_ ->
+					{deny, "Invalid Password"}
+			end
+	end.
+
+%% @doc Retrieve user information from mongo
+handle_userinfo(Username, State) ->
+	case get_account_info(Username, State) of
+		{error, Reason} ->
+			undefined;
+
+		Account ->
+			?info("Got Account Record: ~p", [Account]),
+			Account
+	end.
+
+%TODO: This should be moved out into a header.
+%% @doc Start an application, including it's dependancies
 start_app(App) ->
 	case application:start(App) of
 		ok -> ok;
@@ -97,5 +148,47 @@ start_app(App) ->
 			start_app(App)
 	end.
 
-%TODO: Do something.
+%% @doc Gets a connection to the mongo database
+get_db(Pool) ->
+	case resource_pool:get (Pool) of
+		{ok, Conn} ->
+			Conn;
+
+		{error, Reason} ->
+			?error("Error connecting to the mongo database: ~p", [Reason]),
+			error
+	end.
+
+%% @doc Retrieve the account record from the database
+get_account_info(Username, State) ->
+	#state{conn_pool = Pool} = State,
+	case get_db(Pool) of
+		error ->
+				{error, "Error contacting database."};
+		Conn ->
+			Results = mongo:do(safe, master, Conn, precursors_server, fun() -> 
+				mongo:find_one(accounts, {name, Username}) 
+			end),
+			
+			case Results of
+				{failure, Reason} ->
+					{error, Reason};
+				{ok, Account} ->
+					Account
+			end
+	end.
+
+%% @doc
+generate_hash(Password, Salt) ->
+	Digest = erlsha2:sha256(<<Password/binary, Salt/binary>>),
+	bin_to_hex_string(Digest).
+
+bin_to_hex_string(Bin) ->
+	bin_to_hex_string(Bin, []).
+
+bin_to_hex_string(<<>>, Acc) ->
+	lists:flatten(lists:reverse(Acc));
+
+bin_to_hex_string(<<First/integer, Rest/binary>>, Acc) ->
+	bin_to_hex_string(Rest, [io_lib:format("~2.16.0b", [First]) | Acc]).
 
