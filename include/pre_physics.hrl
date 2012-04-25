53634eb97fd1b40d3701b24254d82d349c2df202
David H. Bronke <whitelynx@gmail.com>
2012-04-25 00:36:10 -0500

    Corrected default quaternions to actually be correct identity transformation quaternions.

--- include/pre_physics.hrl
+++ include/pre_physics.hrl
@@ -3,8 +3,8 @@
 	position_vel = {0, 0, 0} :: vector:vec(),
 	position_acc_abs = {0, 0, 0} :: vector:vec(),
 	position_acc_rel = {0, 0, 0} :: vector:vec(),
-	orientation = {0, 0, 0, 0} :: quaternion:quat(),
-	orientation_vel = {0, 0, 0, 0} :: quaternion:quat(),
-	orientation_acc_abs = {0, 0, 0, 0} :: quaternion:quat(),
-	orientation_acc_rel = {0, 0, 0, 0} :: quaternion:quat()
+	orientation = {1, 0, 0, 0} :: quaternion:quat(),
+	orientation_vel = {1, 0, 0, 0} :: quaternion:quat(),
+	orientation_acc_abs = {1, 0, 0, 0} :: quaternion:quat(),
+	orientation_acc_rel = {1, 0, 0, 0} :: quaternion:quat()
 }).
