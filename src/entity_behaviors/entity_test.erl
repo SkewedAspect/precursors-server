eb7878ff9af6064075fc3640a6077e9174881ffe
David H. Bronke <whitelynx@gmail.com>
2012-05-02 00:27:20 -0500

    Added flying in a circle as a possible starting configuration for the entity.

--- src/entity_behaviors/entity_test.erl
+++ src/entity_behaviors/entity_test.erl
@@ -15,19 +15,38 @@
 init(EntityID, Behavior) ->
 	InitialEntity = entity_physical:init(EntityID, Behavior),
 	InitialPhysical = InitialEntity#entity.physical,
-	InitialEntity#entity{
-		physical = InitialPhysical#physical{
-			position = {random:uniform() * 200 - 100, 700, 10},
-			orientation_vel = quaternion:from_axis_angle(
-				vector:unit({
-					random:uniform(),
-					random:uniform(),
-					random:uniform()
-					}),
-				random:uniform() * math:pi()
-				)
-		}
-	}.
+	Choice = random:uniform(),
+	if
+		Choice < 0.5 ->
+			% Flying in a circle.
+			InitialEntity#entity{
+				physical = InitialPhysical#physical{
+					position = {100, 500, -10},
+					position_vel = {0, 30, 0},
+					position_acc_abs = {0, 0, 0},
+					position_acc_rel = {-9, 0, 0},
+					orientation = {1, 0, 0, 0},
+					orientation_vel = {0.9887710779360422, 0.0, 0.0, 0.14943813247359922},
+					orientation_acc_abs = {1, 0, 0, 0},
+					orientation_acc_rel = {1, 0, 0, 0}
+				}
+			};
+		true ->
+			% Rotating in place.
+			InitialEntity#entity{
+				physical = InitialPhysical#physical{
+					position = {random:uniform() * 200 - 100, 700, 10},
+					orientation_vel = quaternion:from_axis_angle(
+						vector:unit({
+							random:uniform(),
+							random:uniform(),
+							random:uniform()
+							}),
+						random:uniform() * math:pi()
+						)
+				}
+			}
+	end.
 
 %% -------------------------------------------------------------------
 
