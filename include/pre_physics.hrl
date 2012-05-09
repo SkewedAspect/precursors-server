784055e1898bd7abf964455313ab83ded834d3bf
David H. Bronke <whitelynx@gmail.com>
2012-05-08 23:41:13 -0500

    Updated #physics{} record to the new one from pre_physics_rk4.

--- include/pre_physics.hrl
+++ include/pre_physics.hrl
@@ -1,11 +1,25 @@
 -record(physical, {
-	last_update :: erlang:timestamp(),
+	% Updated values (assume these change every frame)
 	position = {0, 0, 0} :: vector:vec(),
-	position_vel = {0, 0, 0} :: vector:vec(),
-	position_acc_abs = {0, 0, 0} :: vector:vec(),
-	position_acc_rel = {0, 0, 0} :: vector:vec(),
+	linear_momentum = {0, 0, 0} :: vector:vec(),
 	orientation = {1, 0, 0, 0} :: quaternion:quat(),
-	orientation_vel = {1, 0, 0, 0} :: quaternion:quat(),
-	orientation_acc_abs = {1, 0, 0, 0} :: quaternion:quat(),
-	orientation_acc_rel = {1, 0, 0, 0} :: quaternion:quat()
+	angular_momentum = {0, 0, 0} :: vector:vec(),
+
+	% Input-only values
+	force_absolute = {0, 0, 0} :: vector:vec(),
+	force_relative = {0, 0, 0} :: vector:vec(),
+	torque_absolute = {0, 0, 0} :: vector:vec(),
+	torque_relative = {0, 0, 0} :: vector:vec(),
+
+	% Purely calculated values (DON'T try to change these externally)
+	last_update :: erlang:timestamp(),
+	linear_velocity = {0, 0, 0} :: vector:vec(),
+	angular_velocity = {0, 0, 0} :: vector:vec(),
+	spin = {1, 0, 0, 0} :: quaternion:quat(),
+
+	% Intrinsic values (should NOT change during the life of an object)
+	mass = 1 :: float(),
+	inverse_mass = 1 :: float(),
+	inertia_tensor = 1 :: float(),
+	inverse_inertia_tensor = 1 :: float()
 }).
