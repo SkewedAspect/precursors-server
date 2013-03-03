8f92a8cec9aab2f8a5d48154f1df5321f40fb201
David H. Bronke <whitelynx@gmail.com>
2013-03-02 20:27:11 -0600

    Corrected spelling and extra quotes.

--- src/pre_util.erl
+++ src/pre_util.erl
@@ -19,21 +19,21 @@
 %% External API
 %% ----------------------------------------------------------------------------
 
-%% @doc Start an application and all of it's required dependancies. This simply
+%% @doc Start an application and all of it's required dependencies. This simply
 %% tries to start the application, and if it fails, it catches the error and if
-%% the error is an unstarted dependancy, it attempts to start that application.
+%% the error is an unstarted dependency, it attempts to start that application.
 start_app(App) ->
 	case application:start(App) of
 		ok ->
-			?info("Application '~p' started", [App]),
+			?info("Application ~p started", [App]),
 			ok;
 
 		{error, {already_started, App}} ->
-			?error("Application '~p' already started.", [App]),
+			?error("Application ~p already started.", [App]),
 			ok;
-		
+
 		{error, {not_started, Dependency}} ->
-			?debug("Unstarted Dependancy '~p' detected, starting...", [Dependency]),
+			?debug("Unstarted Dependency ~p detected, starting...", [Dependency]),
 			ok = start_app(Dependency),
 			start_app(App)
 	end.
@@ -44,6 +44,6 @@ start_apps([]) ->
 	ok;
 
 start_apps([App | Rest]) ->
-	?info("Starting application '~p'.", [App]),
+	?info("Starting application ~p.", [App]),
 	start_app(App),
 	start_apps(Rest).
