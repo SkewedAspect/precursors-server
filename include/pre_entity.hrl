4a9545136c9f10d940b0070399eb56a9869aa66a
Christopher S. Case <chris.case@g33xnexus.com>
2013-03-19 08:46:07 -0500

    removed old entity record. This breaks compile.

--- include/pre_entity.hrl
+++ include/pre_entity.hrl
@@ -1,11 +1,10 @@
 -include("pre_client.hrl").
--include("pre_physics.hrl").
 
 -record(entity, {
-	id :: #entity_id{},
+	id :: binary(),
 	client :: #client_info{},
-	physical = #physical{} :: #physical{},
-	model_def = [{model, <<"Ships/ares">>}] :: json_object(),
-	callback_module = entity_test :: module(),
-	behavior_data :: any()
+	behavior :: atom(),
+	definition :: {Bucket :: binary(), Key :: binary()},
+	state :: json(),
+	watchers :: [pid()]
 }).
