82423076ed90c7b578c4b6bd0a61147057e70791
Christopher S. Case <chris.case@g33xnexus.com>
2013-03-19 09:37:14 -0500

    Added client_request and client_event to interface. Also added explanitory comments.

--- src/entity_behaviors/entity_behavior.erl
+++ src/entity_behaviors/entity_behavior.erl
@@ -1,4 +1,16 @@
-%%% @doc The behavior for all of our entity behavior modules. Provides a basic interface that we can count on.
+%%% @doc The behavior for all of our entity behavior modules.
+%%%
+%%% `simulate` is called every simulation frame by the entity engine. It is expected to return a the new entity record
+%%% that should be used for future simulation. (This is how we modify the entity engine's state to include the updated
+%%% entity.)
+%%%
+%%% `client_request` is called by the entity/client interface (pre_entity_interface) when an request comes in that the
+%%% behavior needs to process. It is expected that this return an appropriate response for the client.
+%%%
+%%% `client_event` is called by the entity/client interface (pre_entity_interface) when an event comes in that the
+%%% behavior needs to process. No response is expected.
+%%%
+%%% --------------------------------------------------------------------------------------------------------------------
 
 -module(entity_behavior).
 -export([behaviour_info/1]).
@@ -8,7 +20,7 @@
 %% --------------------------------------------------------------------------------------------------------------------
 
 behaviour_info(callbacks) ->
-    [{simulate, 1}];
+	[{simulate, 1}, {client_request, 5}, {client_event, 5}];
 
 behaviour_info(_) ->
     undefined.
