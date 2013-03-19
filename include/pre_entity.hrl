57137a72dcf66bb32b0e29c94515a77afcdde9ed
Christopher S. Case <chris.case@g33xnexus.com>
2013-03-19 16:09:56 -0500

    added model to entity definition.

--- include/pre_entity.hrl
+++ include/pre_entity.hrl
@@ -5,6 +5,7 @@
 	client :: #client_info{},
 	behavior :: atom(),
 	definition :: {Bucket :: binary(), Key :: binary()},
+	model = [{model, <<"Ships/ares">>}] :: json(),
 	state :: json(),
 	watchers :: [pid()]
 }).
