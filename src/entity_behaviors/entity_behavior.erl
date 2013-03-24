c3e7255a17a60f7b91743ddc6338c3e162fa82af
David H. Bronke <whitelynx@gmail.com>
2013-03-24 08:22:24 -0500

    Minor change to doc comment formatting, since vim highlights it this way.

--- src/entity_behaviors/entity_behavior.erl
+++ src/entity_behaviors/entity_behavior.erl
@@ -1,16 +1,16 @@
 %%% @doc The behavior for all of our entity behavior modules.
 %%%
-%%% `simulate(EntityRecord, EntityEngineState)` is called every simulation frame by the entity engine. It is expected
+%%% `simulate(EntityRecord, EntityEngineState)' is called every simulation frame by the entity engine. It is expected
 %%% to return a the new entity record that should be used for future simulation. (This is how we modify the entity
 %%% engine's state to include the updated entity.)
-%%% 
-%%% `get_full_update(EntityRecord)` is called whenever a full JSON update message is required. It is expected to return
+%%%
+%%% `get_full_update(EntityRecord)' is called whenever a full JSON update message is required. It is expected to return
 %%% a JSON structure representing the entity's current state.
 %%%
-%%% `client_request` is called by the entity/client interface (pre_entity_interface) when an request comes in that the
+%%% `client_request' is called by the entity/client interface (pre_entity_interface) when an request comes in that the
 %%% behavior needs to process. It is expected that this return an appropriate response for the client.
 %%%
-%%% `client_event` is called by the entity/client interface (pre_entity_interface) when an event comes in that the
+%%% `client_event' is called by the entity/client interface (pre_entity_interface) when an event comes in that the
 %%% behavior needs to process. No response is expected.
 %%%
 %%% -------------------------------------------------------------------------------------------------------------------
