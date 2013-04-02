f24a7e7ca1e16fdddb90d8c0d423d6ac8e5eff3d
Christopher S. Case <chris.case@g33xnexus.com>
2013-04-02 11:38:04 -0500

    converted entity_ship to the new behavior format. Now it takes whatever state it was passed, and only updates where something isn't defined, instead of forcing to defaults. This means (in theory) we should be fully compatible with loading from the database.

--- src/entity_behaviors/entity_ship.erl
+++ src/entity_behaviors/entity_ship.erl
@@ -32,182 +32,228 @@
 
 init(EntityID, Behavior) ->
 	InitialEntity = entity_physical:init(EntityID, Behavior),
-	InitialPhysical = proplists:get_value(physical, InitialEntity#entity.state),
-	% Random initial position and orientation.
+
+	% -----------------------------------------------------------------------------------------------------------------
+
+	% Setup initial physical state
+	InitialPhysical = dict:fetch(physical, InitialEntity#entity.state),
+
+	% Setup default physical state with random position/orientation
+	DefaultPhysical = dict:from_list([
+		{
+			position, {
+				random:uniform() * 200 - 100,
+				random:uniform() * 200 + 600,
+				random:uniform() * 20
+			}
+		},
+		{
+			orientation, quaternion:from_axis_angle(
+				vector:unit({
+					random:uniform(),
+					random:uniform(),
+					random:uniform()
+				}),
+				random:uniform() * math:pi()
+			)
+		}
+	]),
+
+	% Merge our initial physical state dict with our default values, prefering our initials where there's
+	% conflicts.
+	Physical = dict:merge(fun(_Key, InitialVal, _DefaultVal) ->
+		InitialVal
+	end, InitialPhysical, DefaultPhysical),
+
+	% -----------------------------------------------------------------------------------------------------------------
+
+	% Setup ship state
+	InitialShip = dict:fetch(ship, InitialEntity#entity.state),
+
+	DefaultShip = dict:from_list([
+		{ target_linear_velocity, {0, 0, 0} },
+		{ target_angular_velocity, {0, 0, 0} },
+
+		% Intrinsic ship parameters
+		{ linear_target_velocity_scaling, {600, 800, 500} }, % {sideslip, throttle, lift}
+		{ angular_target_velocity_scaling, {2, 2, 2} }, %  {pitch, roll, yaw}
+		{ max_linear_thrust, {300, 400, 250} }, % {sideslip, throttle, lift}
+		{ max_angular_thrust, {2, 2, 2} }, %  {pitch, roll, yaw}
+		{ linear_responsiveness, {3, 3, 3} }, % {sideslip, throttle, lift}
+		{ angular_responsiveness, {3, 3, 3} } %  {pitch, roll, yaw}
+	]),
+
+	% Merge our initial shipstate dict with our default values, prefering our initials where there's
+	% conflicts.
+	Ship = dict:merge(fun(_Key, InitialVal, _DefaultVal) ->
+		InitialVal
+	end, InitialShip, DefaultShip),
+
+	% -----------------------------------------------------------------------------------------------------------------
+
+	% Set up our initial state
+	InitialState = InitialEntity#entity.state,
+	State1 = dict:store(physical, Physical, InitialState),
+	State2 = dict:store(ship, Ship, State1),
+
 	InitialEntity#entity{
-		state = lists:keystore(physical, 1,
-			lists:keystore(ship, 1, InitialEntity#entity.state, {ship, #ship_data{}}),
-			{physical, InitialPhysical#physical{
-				position = {random:uniform() * 200 - 100, random:uniform() * 200 + 600, random:uniform() * 20},
-				orientation = quaternion:from_axis_angle(
-					vector:unit({
-						random:uniform(),
-						random:uniform(),
-						random:uniform()
-					}),
-					random:uniform() * math:pi()
-				)
-			}}
-		)
+		state = State2
 	}.
 
 %% --------------------------------------------------------------------------------------------------------------------
 
-get_full_state(EntityState) ->
-	entity_physical:get_full_state(EntityState).
+get_full_state(Entity) ->
+	entity_physical:get_full_state(Entity).
 
 %% --------------------------------------------------------------------------------------------------------------------
 
-client_request(EntityState, input, <<"command">>, _RequestID, Request) ->
-	handle_input_command(EntityState, Request);
+client_request(Entity, input, <<"command">>, _RequestID, Request) ->
+	handle_input_command(Entity, Request);
 
-client_request(EntityState, Channel, RequestType, RequestID, Request) ->
-	entity_physical:client_request(EntityState, Channel, RequestType, RequestID, Request).
-
-%% --------------------------------------------------------------------------------------------------------------------
-
-client_event(EntityState, _ClientInfo, input, <<"command">>, Event) ->
-	{_Response, EntityState1} = handle_input_command(EntityState, Event),
-	{noreply, EntityState1};
-
-client_event(EntityState, ClientInfo, Channel, EventType, Event) ->
-	entity_physical:client_event(EntityState, ClientInfo, Channel, EventType, Event).
+client_request(Entity, Channel, RequestType, RequestID, Request) ->
+	entity_physical:client_request(Entity, Channel, RequestType, RequestID, Request).
 
 %% --------------------------------------------------------------------------------------------------------------------
 
-simulate(EntityState, EntityEngineState) ->
-	EntityState1 = do_flight_control(EntityState),
-	entity_physical:simulate(EntityState1, EntityEngineState).
+client_event(Entity, _ClientInfo, input, <<"command">>, Event) ->
+	{_Response, Entity1} = handle_input_command(Entity, Event),
+	{noreply, Entity1};
+
+client_event(Entity, ClientInfo, Channel, EventType, Event) ->
+	entity_physical:client_event(Entity, ClientInfo, Channel, EventType, Event).
 
 %% --------------------------------------------------------------------------------------------------------------------
 
-handle_input_command(EntityState, [{_, _} | _] = RawCommand) ->
+simulate(Entity, EntityEngineState) ->
+	Entity1 = do_flight_control(Entity),
+	entity_physical:simulate(Entity1, EntityEngineState).
+
+%% --------------------------------------------------------------------------------------------------------------------
+
+handle_input_command(Entity, [{_, _} | _] = RawCommand) ->
 	Command = proplists:get_value(name, RawCommand),
 	Args = proplists:get_value(args, RawCommand),
 	KWArgs = proplists:get_value(kwargs, RawCommand),
-	handle_input_command(EntityState, Command, Args, KWArgs).
+	handle_input_command(Entity, Command, Args, KWArgs).
 
 %% --------------------------------------------------------------------------------------------------------------------
 
 % Positional target velocity
-handle_input_command(EntityState, <<"sideslip">>, [TargetVel], _KWArgs) ->
-	set_target_linear_velocity(EntityState, {TargetVel, undefined, undefined});
+handle_input_command(Entity, <<"sideslip">>, [TargetVel], _KWArgs) ->
+	set_target_linear_velocity(Entity, {TargetVel, undefined, undefined});
 
-handle_input_command(EntityState, <<"throttle">>, [TargetVel], _KWArgs) ->
-	set_target_linear_velocity(EntityState, {undefined, TargetVel, undefined});
+handle_input_command(Entity, <<"throttle">>, [TargetVel], _KWArgs) ->
+	set_target_linear_velocity(Entity, {undefined, TargetVel, undefined});
 
-handle_input_command(EntityState, <<"lift">>, [TargetVel], _KWArgs) ->
-	set_target_linear_velocity(EntityState, {undefined, undefined, TargetVel});
+handle_input_command(Entity, <<"lift">>, [TargetVel], _KWArgs) ->
+	set_target_linear_velocity(Entity, {undefined, undefined, TargetVel});
 
 % Rotational target velocity
-handle_input_command(EntityState, <<"pitch">>, [TargetVel], _KWArgs) ->
-	set_target_angular_velocity(EntityState, {TargetVel, undefined, undefined});
+handle_input_command(Entity, <<"pitch">>, [TargetVel], _KWArgs) ->
+	set_target_angular_velocity(Entity, {TargetVel, undefined, undefined});
 
-handle_input_command(EntityState, <<"roll">>, [TargetVel], _KWArgs) ->
-	set_target_angular_velocity(EntityState, {undefined, TargetVel, undefined});
+handle_input_command(Entity, <<"roll">>, [TargetVel], _KWArgs) ->
+	set_target_angular_velocity(Entity, {undefined, TargetVel, undefined});
 
-handle_input_command(EntityState, <<"yaw">>, [TargetVel], _KWArgs) ->
-	set_target_angular_velocity(EntityState, {undefined, undefined, TargetVel});
+handle_input_command(Entity, <<"yaw">>, [TargetVel], _KWArgs) ->
+	set_target_angular_velocity(Entity, {undefined, undefined, TargetVel});
 
 % Catch-all
-handle_input_command(EntityState, Command, Args, KWArgs) ->
+handle_input_command(Entity, Command, Args, KWArgs) ->
 	?info("Got unrecognized input command: ~p, ~p, ~p", [Command, Args, KWArgs]),
 	Response = {reply, [
 		{confirm, false},
-		{reason, <<"VALID CRAPBACK: Unrecognized input command \"", Command/binary, "\"!">>}
+		{reason, <<"Unrecognized input command \"", Command/binary, "\"!">>}
 	]},
-	{Response, EntityState}.
+	{Response, Entity}.
 
 %% --------------------------------------------------------------------------------------------------------------------
 
-set_target_angular_velocity(EntityState, New) ->
-	%?debug("Setting orientation velocity to ~p.", [New]),
-	ShipData = proplists:get_value(ship, EntityState#entity.state),
-	#ship_data{
-		target_angular_velocity = Current
-	} = ShipData,
+set_target_angular_velocity(Entity, NewAngVel) ->
+	%?debug("Setting orientation velocity to ~p.", [NewAngVel]),
+	ShipState = dict:fetch(ship, Entity#entity.state),
+	CurrentAngVel = dict:fetch(target_angular_velocity, ShipState),
 
-	EntityState1 = EntityState#entity{
-		state = lists:keystore(ship, 1, EntityState#entity.state, {ship,
-			ShipData#ship_data{
-				target_angular_velocity = update_vector(New, Current)
-			}
-		})
+	% Update the target angular velocity
+	NewShipState = dict:store(target_angular_velocity, update_vector(NewAngVel, CurrentAngVel), ShipState),
+
+	% Update State
+	Entity1 = Entity#entity{
+		state = dict:store(ship, NewShipState, Entity#entity.state)
 	},
-	Response = {reply, [
-		{confirm, true}
-	]},
-	{Response, EntityState1}.
+
+	% Build response
+	Response = {reply, [{confirm, true}]},
+
+	{Response, Entity1}.
 
 %% --------------------------------------------------------------------------------------------------------------------
 
-set_target_linear_velocity(EntityState, New) ->
-	%?debug("Setting position velocity to ~p.", [New]),
-	ShipData = proplists:get_value(ship, EntityState#entity.state),
-	#ship_data{
-		target_angular_velocity = Current
-	} = ShipData,
+set_target_linear_velocity(Entity, NewLinVel) ->
+	%?debug("Setting position velocity to ~p.", [NewLinVel]),
+	ShipState = dict:fetch(ship, Entity#entity.state),
+	CurrentLinVel = dict:fetch(target_linear_velocity, ShipState),
 
-	EntityState1 = EntityState#entity{
-		state = lists:keystore(ship, 1, EntityState#entity.state, {ship,
-			ShipData#ship_data{
-				target_linear_velocity = update_vector(New, Current)
-			}
-		})
+	% Update the target linear velocity
+	NewShipState = dict:store(target_linear_velocity, update_vector(NewLinVel, CurrentLinVel), ShipState),
+
+	% Update State
+	Entity1 = Entity#entity{
+		state = dict:store(ship, NewShipState, Entity#entity.state)
 	},
-	Response = {reply, [
-		{confirm, true}
-	]},
-	{Response, EntityState1}.
+
+	% Build response
+	Response = {reply, [{confirm, true}]},
+
+	{Response, Entity1}.
 
 %% --------------------------------------------------------------------------------------------------------------------
 
-do_flight_control(EntityState) ->
-	Physical = proplists:get_value(physical, EntityState#entity.state),
-	#physical{
-		orientation = Orientation,
-		linear_velocity = PositionVelAbs,
-		% Since Euler vectors are {X, Y, Z} where unit({X, Y, Z}) is the axis of rotation and magnitude({X, Y, Z})
-		% is the speed of rotation, that works out to {1, 0, 0} being pitch (rotation around X), {0, 1, 0} being
-		% roll (rotation around Y), etc. Therefore, decomposing each of the rotations gives us {Pitch, Roll, Yaw}.
-		angular_velocity = AngularVelAbs
-	} = Physical,
+do_flight_control(Entity) ->
+	Physical = dict:fetch(physical, Entity#entity.state),
+	Orientation = dict:fetch(orientation, Physical),
+	PositionVelAbs = dict:fetch(linear_velocity, Physical),
+	AngularVelAbs = dict:fetch(angular_velocity, Physical),
 
-	ShipData = proplists:get_value(ship, EntityState#entity.state),
-	#ship_data{
-		target_linear_velocity = {TX, TY, TZ},
-		target_angular_velocity = {TPitch, TRoll, TYaw},
-		linear_target_velocity_scaling = {TXScale, TYScale, TZScale},
-		angular_target_velocity_scaling = {TPitchScale, TRollScale, TYawScale},
-		max_linear_thrust = {MaxXT, MaxYT, MaxZT},
-		max_angular_thrust = {MaxPitchT, MaxRollT, MaxYawT},
-		linear_responsiveness = {XR, YR, ZR},
-		angular_responsiveness = {PitchR, RollR, YawR}
-	} = ShipData,
+	ShipState = dict:fetch(ship, Entity#entity.state),
+	{TX, TY, TZ} = dict:fetch(target_linear_velocity, ShipState),
+	{TPitch, TRoll, TYaw} = dict:fetch(target_angular_velocity, ShipState),
+	{TXScale, TYScale, TZScale} = dict:fetch(linear_target_velocity_scaling, ShipState),
+	{TPitchScale, TRollScale, TYawScale} = dict:fetch(angular_target_velocity_scaling, ShipState),
+	{MaxXT, MaxYT, MaxZT} = dict:fetch(max_linear_thrust, ShipState),
+	{MaxPitchT, MaxRollT, MaxYawT} = dict:fetch(max_angular_thrust, ShipState),
+	{XR, YR, ZR} = dict:fetch(linear_responsiveness, ShipState),
+	{PitchR, RollR, YawR} = dict:fetch(angular_responsiveness, ShipState),
 
+	% Get the ship-relative linear velocity
 	{XVel, YVel, ZVel} = quaternion:rotate(PositionVelAbs, quaternion:reciprocal(Orientation)),
+
+	% Get the ship-relative angular velocity
 	{PitchVel, RollVel, YawVel} = quaternion:rotate(AngularVelAbs, quaternion:reciprocal(Orientation)),
 
+	% Calculate relative force
 	Force = {
 		calc_thrust(MaxXT, XR, XVel, TXScale * TX),
 		calc_thrust(MaxYT, YR, YVel, TYScale * TY),
 		calc_thrust(MaxZT, ZR, ZVel, TZScale * TZ)
 	},
 
+	% Calculate relative torque
 	Torque = {
 		calc_thrust(MaxPitchT, PitchR, PitchVel, TPitchScale * TPitch),
 		calc_thrust(MaxRollT, RollR, RollVel, TRollScale * TRoll),
 		calc_thrust(MaxYawT, YawR, YawVel, TYawScale * TYaw)
 	},
 
-	EntityState#entity{
-		state = lists:keystore(physical, 1, EntityState#entity.state,
-			{physical, Physical#physical{
-				force_relative = Force,
-				torque_relative = Torque
-			}}
-		)
+	% Update physical state
+	NewPhysical1 = dict:store(force_relative, Force, Physical),
+	NewPhysical2 = dict:store(torque_relatice, Torque, NewPhysical1),
+
+	% Update entity state
+	Entity#entity{
+		state = dict:store(physical, NewPhysical2, Entity#entity.state)
 	}.
+
 
 calc_thrust(MaxTh, Resp, CurVel, TargetVel) ->
 	DMToP = 2 * MaxTh / math:pi(),
