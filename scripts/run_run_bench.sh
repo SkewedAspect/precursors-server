07e039c670a0f51ab007502837a62b661fb411b0
David H. Bronke <whitelynx@gmail.com>
2013-04-03 00:41:07 -0500

    Added comments, added 'set -e' to exit on all errors, and added highlighting of table headers.

--- scripts/run_run_bench.sh
+++ scripts/run_run_bench.sh
@@ -2,9 +2,14 @@
 
 # Run run_bench.escript, coloring the output according to the type of operation.
 
+# Exit on all errors.
+set -e
+
+# Recompile, just in case.
 erlc -smp gen_bench.erl
 
 escript run_bench.escript | awk '
+/Operation *Total \(us\) *Average \(us\)/ {print "\033[1m" $0 "\033[m"; next}
 $1 ~ /^lookup|^get|^fetch/ {print "\033[1;32m" $0 "\033[m"; next}
 $1 ~ /^iterate_/ {print "\033[33m" $0 "\033[m"; next}
 $1 ~ /^store|^insert|^keystore/ {print "\033[1;34m" $0 "\033[m"; next}
