5c66261254ce6cd9a2f65a04e9c1009d6aea0b74
David H. Bronke <whitelynx@gmail.com>
2013-04-02 23:36:56 -0500

    Added run_run_bench.sh, to run run_bench.escript with output colorization.

---
+++ scripts/run_run_bench.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Run run_bench.escript, coloring the output according to the type of operation.
+
+erlc -smp gen_bench.erl
+
+escript run_bench.escript | awk '
+$1 ~ /^lookup|^get|^fetch/ {print "\033[1;32m" $0 "\033[m"; next}
+$1 ~ /^iterate_/ {print "\033[33m" $0 "\033[m"; next}
+$1 ~ /^store|^insert|^keystore/ {print "\033[1;34m" $0 "\033[m"; next}
+$1 ~ /^delete|^erase/ {print "\033[35m" $0 "\033[m"; next}
+{print}
+'
