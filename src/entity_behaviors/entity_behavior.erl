ff9266ffe2a1ace38ef9ec8d07ec8dfe1c526aab
David H. Bronke <whitelynx@gmail.com>
2013-04-02 23:01:34 -0500

    Updated comments to list arguments for client_request/5 and client_event/5, and added entity_event/3.

--- src/entity_behaviors/entity_behavior.erl
+++ src/entity_behaviors/entity_behavior.erl
@@ -1,17 +1,22 @@
 %%% @doc The behavior for all of our entity behavior modules.
 %%%
-%%% `simulate(EntityRecord, EntityEngineState)' is called every simulation frame by the entity engine. It is expected
-%%% to return a the new entity record that should be used for future simulation. (This is how we modify the entity
-%%% engine's state to include the updated entity.)
+%%% `simulate(Entity, EntityEngineState)' is called every simulation frame by the entity engine. It is expected
+%%% to return `{Update, NewEntity}` where `Update` is either a JSON structure representing any changes or `undefined`
+%%% if no changes occurred, and `NewEntity` is the new entity record that should be used for future simulation.
 %%%
-%%% `get_full_update(EntityRecord)' is called whenever a full JSON update message is required. It is expected to return
+%%% `get_full_update(Entity)' is called whenever a full JSON update message is required. It is expected to return
 %%% a JSON structure representing the entity's current state.
 %%%
-%%% `client_request' is called by the entity/client interface (pre_entity_interface) when an request comes in that the
-%%% behavior needs to process. It is expected that this return an appropriate response for the client.
+%%% `client_request(Channel, RequestType, RequestID, Request, Entity)' is called by the entity/client communication
+%%% interface (pre_entity_comm) when the client sends a request that the behavior needs to process. It is expected to
+%%% return `{ok, Response, NewEntity}` where `Response` is a JSON structure to send back to the client.
 %%%
-%%% `client_event' is called by the entity/client interface (pre_entity_interface) when an event comes in that the
-%%% behavior needs to process. No response is expected.
+%%% `client_event(ClientInfo, Channel, EventType, Event, Entity)' is called by the entity/client communication
+%%% interface (pre_entity_comm) when the client sends an event that the behavior needs to process. No response is
+%%% expected.
+%%%
+%%% `entity_event(Event, From, Entity)' is called by the entity engine whenever another entity sends or broadcasts an
+%%% event. No response is expected.
 %%%
 %%% -------------------------------------------------------------------------------------------------------------------
 
@@ -23,7 +28,7 @@
 %% --------------------------------------------------------------------------------------------------------------------
 
 behaviour_info(callbacks) ->
-	[{simulate, 2}, {get_full_state, 1}, {client_request, 5}, {client_event, 5}];
+	[{simulate, 2}, {get_full_state, 1}, {client_request, 5}, {client_event, 5}, {entity_event, 3}];
 
 behaviour_info(_) ->
     undefined.
