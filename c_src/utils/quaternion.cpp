0de02b5b78270cb4f0b7ba14ce156dbd816ba5f8
David H. Bronke <whitelynx@gmail.com>
2013-04-06 13:51:12 -0500

    Moved list of functions exported by the Erlang module into the header, and made it a bit clearer what the status of the C++ implementations and wrapper functions are.

--- c_src/utils/quaternion.cpp
+++ c_src/utils/quaternion.cpp
@@ -225,35 +225,6 @@ static Quat operator*(const double lhs, const Quat& rhs)
 /* ====================================================================================================================
  * Erlang API
  * ================================================================================================================= */
-/* Exports: (% => leave implemented in Erlang; - => implement in C)
- * % quat_to_list/1
- * % list_to_quat/1
- * - add/2
- * - subtract/2
- * - multiply/2
- * - divide/2
- * - reorient/2
- * - scale_rotation/2
- * - norm/1
- * - length/1
- * - unit/1
- * - conjugate/1
- * - inverse/1
- * - reciprocal/1
- * - compose/2
- * - relative_to/2
- * - rotate/2
- * - from_axis_angle/2
- * - from_axis_angle/3
- * - from_body_rates/1
- * - from_body_rates/2
- * - from_euler/1
- * - from_euler/2
- * - rad2deg/1
- * - deg2rad/1
- * - is_zero/1
- */
-
 
 #if 0 // For now, leave these implemented in Erlang; they're probably just as fast there, and not needed in C.
 /**
