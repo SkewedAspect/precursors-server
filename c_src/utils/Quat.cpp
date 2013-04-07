0ce59e4acb127b41f67c4cfc31f954705b5e84f8
David H. Bronke <whitelynx@gmail.com>
2013-04-07 01:15:37 -0500

    Split quaternion into Quat and quaternion_nif files, and got skeleton implementations of both the Quat class and the NIF module done; the NIF module builds and loads.

---
+++ c_src/utils/Quat.cpp
@@ -0,0 +1,285 @@
+/**
+ * @doc The Quat class
+ *
+ * @copyright 2012-2013 David H. Bronke and Christopher S. Case
+ * Licensed under the MIT license; see the LICENSE file for details.
+ */
+
+#include <cmath>
+
+#include <erl_nif.h>
+
+#include "angles.h"
+
+#include "Quat.h"
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// Some useful constants
+
+#define NORMALIZED_TOLERANCE 0.0000001
+
+
+// --------------------------------------------------------------------------------------------------------------------
+
+Quat::Quat() :
+	w(0), x(0), y(0), z(0)
+{
+} // end Quat
+
+Quat::Quat(double w, double x, double y, double z) :
+	w(w), x(x), y(y), z(z)
+{
+} // end Quat
+
+
+// ----------------------------------------------------------------------------------------------------------------
+// Operations that produce new `Quat` instances
+
+/// Perform a cross product with the given Quat.
+Quat Quat::cross(const Quat& other) const
+{
+} // end cross
+
+/// Returns a unit quaternion in the same direction as this Quat.
+Quat Quat::unit() const
+{
+} // end unit
+
+/// Returns a new Quat that is the conjugate of this one.
+Quat Quat::conjugate() const
+{
+} // end conjugate
+
+/// Returns a new Quat that is the inverse of this one.
+Quat Quat::inverse() const
+{
+} // end inverse
+
+/// Returns a new Quat that is the reciprocal of this one.
+Quat Quat::reciprocal() const
+{
+} // end reciprocal
+
+/// Returns a new Quat which results from composing this Quat's rotation with the given Quat.
+Quat Quat::compose(const Quat& other) const
+{
+} // end compose
+
+/// Get the Quat representing the orientation of this Quat relative to the given one.
+Quat Quat::relativeTo(const Quat& other) const
+{
+} // end relativeTo
+
+
+// Operator overloads (Quat <op> Quat)
+
+Quat Quat::operator +(const Quat& other) const
+{
+} // end operator +
+
+Quat Quat::operator -(const Quat& other) const
+{
+} // end operator -
+
+Quat Quat::operator *(const Quat& other) const
+{
+} // end operator *
+
+
+// Operator overloads (Quat <op> <other type>)
+
+Quat Quat::operator *(const double& factor) const
+{
+} // end operator *
+
+Quat Quat::operator *(const int32_t& factor) const
+{
+} // end operator *
+
+Quat Quat::operator *(const u_int32_t& factor) const
+{
+} // end operator *
+
+Quat Quat::operator *(const int64_t& factor) const
+{
+} // end operator *
+
+Quat Quat::operator *(const u_int64_t& factor) const
+{
+} // end operator *
+
+Quat Quat::operator /(const double& divisor) const
+{
+} // end operator /
+
+Quat Quat::operator /(const int32_t& divisor) const
+{
+} // end operator /
+
+Quat Quat::operator /(const u_int32_t& divisor) const
+{
+} // end operator /
+
+Quat Quat::operator /(const int64_t& divisor) const
+{
+} // end operator /
+
+Quat Quat::operator /(const u_int64_t& divisor) const
+{
+} // end operator /
+
+
+// ----------------------------------------------------------------------------------------------------------------
+// Operations that produce other values
+
+/// Returns the squared length of the quaternion. This is useful in some optimization cases, as it avoids a sqrt call.
+double Quat::squaredNorm() const
+{
+} // end squaredNorm
+
+/// Returns the length of the quaternion.
+double Quat::norm() const
+{
+} // end norm
+
+/// Rotate the given vector by this Quat's rotation.
+Vec Quat::rotate(const Vec& other) const
+{
+} // end rotate
+
+/// Checks to see if this is a zero quaternion.
+bool Quat::isZero() const
+{
+} // end isZero
+
+/// Access components using Quat[idx]
+double Quat::operator [](const size_t& idx) const throw(BadIndex<size_t>)
+{
+} // end operator []
+
+
+// ----------------------------------------------------------------------------------------------------------------
+// In-place modifications
+
+Quat& Quat::normalize()
+{
+} // end normalize
+
+Quat& Quat::invert()
+{
+} // end invert
+
+
+/// Converts from an axis and angle (degrees) to a quaternion.
+Quat& Quat::fromAxisAngleDeg(const Vec& axis, double angleDeg)
+{
+	return fromAxisAngleRad(axis, deg2rad(angleDeg));
+} // end fromAxisAngleDeg
+
+/// Converts from an axis and angle (radians) to a quaternion.
+Quat& Quat::fromAxisAngleRad(const Vec& axis, double angleRad)
+{
+	double halfAngle = angleRad / 2;
+	double complexFactor = sin(halfAngle);
+
+	w = cos(halfAngle);
+	x = complexFactor * axis.x;
+	y = complexFactor * axis.y;
+	z = complexFactor * axis.z;
+
+	return *this;
+} // end fromAxisAngleRad
+
+/// Converts from body rates (degrees) to a quaternion.
+Quat& Quat::fromBodyRatesDeg(const Vec& bodyRatesDeg)
+{
+	return fromBodyRatesRad(Vec(deg2rad(bodyRatesDeg.x), deg2rad(bodyRatesDeg.y), deg2rad(bodyRatesDeg.z)));
+} // end fromBodyRatesDeg
+
+/// Converts from body rates (radians) to a quaternion.
+Quat& Quat::fromBodyRatesRad(const Vec& bodyRatesRad)
+{
+	if(bodyRatesRad.isZero())
+	{
+		// The identity quaternion
+		w = 1;
+		x = 0;
+		y = 0;
+		z = 0;
+
+		return *this;
+	}
+	else
+	{
+		//FIXME: According to the Erlang code, this is wrong! (not sure whether I entirely believe it)
+		Vec axis(bodyRatesRad.y, bodyRatesRad.z, bodyRatesRad.x);
+
+		double speed = axis.norm();
+		axis /= speed;
+
+		return fromAxisAngleRad(axis, speed);
+	} // end if
+} // end fromBodyRatesRad
+
+/// Converts from a vector of euler angles ((yaw, pitch, roll), in degrees) to a quaternion.
+Quat& Quat::fromEulerDeg(const Vec& anglesDeg)
+{
+	return fromEulerRad(Vec(deg2rad(anglesDeg.x), deg2rad(anglesDeg.y), deg2rad(anglesDeg.z)));
+} // end fromEulerDeg
+
+/// Converts from a vector of euler angles ((yaw, pitch, roll), in radians) to a quaternion.
+Quat& Quat::fromEulerRad(const Vec& anglesRad)
+{
+	double halfYaw = anglesRad.x / 2;
+	double halfPitch = anglesRad.y / 2;
+	double halfRoll = anglesRad.z / 2;
+
+	w = cos(halfPitch) * cos(halfRoll) * cos(halfYaw) + sin(halfPitch) * sin(halfRoll) * sin(halfYaw);
+	x = sin(halfPitch) * cos(halfRoll) * cos(halfYaw) - cos(halfPitch) * sin(halfRoll) * sin(halfYaw);
+	y = cos(halfPitch) * sin(halfRoll) * cos(halfYaw) + sin(halfPitch) * cos(halfRoll) * sin(halfYaw);
+	z = cos(halfPitch) * cos(halfRoll) * sin(halfYaw) + sin(halfPitch) * sin(halfRoll) * cos(halfYaw);
+
+	return *this;
+} // end fromEulerRad
+
+
+// Create a compound assignment operator overload that sets each component of the Quat to `this.coord OP other.coord`.
+#define COMP_ASSIGN_OP_ZIP(OP, OTHER_TYPE) \
+		Quat& Quat::operator OP##=(const OTHER_TYPE& other) \
+		{ \
+			w OP##= other.w; \
+			x OP##= other.x; \
+			y OP##= other.y; \
+			z OP##= other.z; \
+			return *this; \
+		}
+
+// Operator overloads (Quat <op> Quat)
+COMP_ASSIGN_OP_ZIP(+, Quat)
+COMP_ASSIGN_OP_ZIP(-, Quat)
+COMP_ASSIGN_OP_ZIP(*, Quat)
+
+
+// Create a compound assignment operator overload that sets each component of the Quat to `this.coord OP other`.
+#define COMP_ASSIGN_OP_MAP(OP, OTHER_TYPE) \
+		Quat& Quat::operator OP##=(const OTHER_TYPE& other) \
+		{ \
+			w OP##= other; \
+			x OP##= other; \
+			y OP##= other; \
+			z OP##= other; \
+			return *this; \
+		}
+
+// Operator overloads (Quat <op>= <other type>)
+COMP_ASSIGN_OP_MAP(*, double)
+COMP_ASSIGN_OP_MAP(*, int32_t)
+COMP_ASSIGN_OP_MAP(*, u_int32_t)
+COMP_ASSIGN_OP_MAP(*, int64_t)
+COMP_ASSIGN_OP_MAP(*, u_int64_t)
+COMP_ASSIGN_OP_MAP(/, double)
+COMP_ASSIGN_OP_MAP(/, int32_t)
+COMP_ASSIGN_OP_MAP(/, u_int32_t)
+COMP_ASSIGN_OP_MAP(/, int64_t)
+COMP_ASSIGN_OP_MAP(/, u_int64_t)
---
+++ c_src/utils/Quat.h
@@ -0,0 +1,158 @@
+/**
+ * @doc The Quat class
+ *
+ * @copyright 2012-2013 David H. Bronke and Christopher S. Case
+ * Licensed under the MIT license; see the LICENSE file for details.
+ */
+
+#ifndef __QUAT__
+#define __QUAT__
+
+#include "exceptions.h"
+
+#include "Vec.h"
+
+
+#ifdef __GNUC__
+#	define FLATTEN __attribute__((flatten))
+#else
+#	define FLATTEN
+#endif
+
+
+/**
+ * A quaternion. (w + xi + yj + zk)
+ */
+class Quat
+{
+public:
+	Quat();
+
+	Quat(double w, double x, double y, double z);
+
+
+	// ----------------------------------------------------------------------------------------------------------------
+	// Operations that produce new `Quat` instances
+
+	/// Perform a cross product with the given Quat.
+	Quat cross(const Quat& other) const;
+
+	/// Returns a unit quaternion in the same direction as this Quat.
+	Quat unit() const;
+
+	/// Returns a new Quat that is the conjugate of this one.
+	Quat conjugate() const;
+
+	/// Returns a new Quat that is the inverse of this one.
+	Quat inverse() const;
+
+	/// Returns a new Quat that is the reciprocal of this one.
+	Quat reciprocal() const;
+
+	/// Returns a new Quat which results from composing this Quat's rotation with the given Quat.
+	Quat compose(const Quat& other) const;
+
+	/// Get the Quat representing the orientation of this Quat relative to the given one.
+	Quat relativeTo(const Quat& other) const;
+
+	// Operator overloads (Quat <op> Quat)
+	Quat operator +(const Quat& other) const;
+	Quat operator -(const Quat& other) const;
+	Quat operator *(const Quat& other) const;
+
+	// Operator overloads (Quat <op> <other type>)
+	Quat operator *(const double& factor) const;
+	Quat operator *(const int32_t& factor) const;
+	Quat operator *(const u_int32_t& factor) const;
+	Quat operator *(const int64_t& factor) const;
+	Quat operator *(const u_int64_t& factor) const;
+	Quat operator /(const double& divisor) const;
+	Quat operator /(const int32_t& divisor) const;
+	Quat operator /(const u_int32_t& divisor) const;
+	Quat operator /(const int64_t& divisor) const;
+	Quat operator /(const u_int64_t& divisor) const;
+
+
+	// ----------------------------------------------------------------------------------------------------------------
+	// Operations that produce other values
+
+	/// Returns the squared length of the quaternion. This is useful in some optimization cases, as it avoids a sqrt call.
+	double squaredNorm() const;
+
+	/// Returns the length of the quaternion.
+	double norm() const;
+
+	/// Returns the length of the quaternion.
+	inline double FLATTEN length() const { return norm(); }
+
+	/// Rotate the given vector by this Quat's rotation.
+	Vec rotate(const Vec& other) const;
+
+	/// Checks to see if this is a zero quaternion.
+	bool isZero() const;
+
+	/// Access components using Quat[idx]
+	double operator [](const size_t& idx) const throw(BadIndex<size_t>);
+
+
+	// ----------------------------------------------------------------------------------------------------------------
+	// In-place modifications
+
+	Quat& normalize();
+	Quat& invert();
+
+	/// Converts from an axis and angle (degrees) to a quaternion.
+	Quat& fromAxisAngleDeg(const Vec& axis, double angleDeg);
+
+	/// Converts from an axis and angle (radians) to a quaternion.
+	Quat& fromAxisAngleRad(const Vec& axis, double angleRad);
+
+	/// Converts from body rates (degrees) to a quaternion.
+	Quat& fromBodyRatesDeg(const Vec& bodyRatesDeg);
+
+	/// Converts from body rates (radians) to a quaternion.
+	Quat& fromBodyRatesRad(const Vec& bodyRatesRad);
+
+	/// Converts from a vector of euler angles ((yaw, pitch, roll), in degrees) to a quaternion.
+	Quat& fromEulerDeg(const Vec& anglesDeg);
+
+	/// Converts from a vector of euler angles ((yaw, pitch, roll), in radians) to a quaternion.
+	Quat& fromEulerRad(const Vec& anglesRad);
+
+	// Operator overloads (Quat <op>= Quat)
+	Quat& operator +=(const Quat& other);
+	Quat& operator -=(const Quat& other);
+	Quat& operator *=(const Quat& other);
+
+	// Operator overloads (Quat <op>= <other type>)
+	Quat& operator *=(const double& factor);
+	Quat& operator *=(const int32_t& factor);
+	Quat& operator *=(const u_int32_t& factor);
+	Quat& operator *=(const int64_t& factor);
+	Quat& operator *=(const u_int64_t& factor);
+	Quat& operator /=(const double& divisor);
+	Quat& operator /=(const int32_t& divisor);
+	Quat& operator /=(const u_int32_t& divisor);
+	Quat& operator /=(const int64_t& divisor);
+	Quat& operator /=(const u_int64_t& divisor);
+
+
+	// ----------------------------------------------------------------------------------------------------------------
+	// Properties
+
+	// Components
+	double w;
+	double x;
+	double y;
+	double z;
+}; // end Quat
+
+
+// Operator overloads (<other type> <op> Quat)
+Quat operator *(const double& factor, const Quat& quat);
+Quat operator *(const int32_t& factor, const Quat& quat);
+Quat operator *(const u_int32_t& factor, const Quat& quat);
+Quat operator *(const int64_t& factor, const Quat& quat);
+Quat operator *(const u_int64_t& factor, const Quat& quat);
+
+#endif // __QUAT__
