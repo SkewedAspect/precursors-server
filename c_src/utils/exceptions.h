2551ab67885977b2efad9b6f6960cffcb65df35f
David H. Bronke <whitelynx@gmail.com>
2013-04-06 20:12:26 -0500

    Moved the implementation of the BadIndex exception into its header, since templates' implementations need to be available wherever they're instantiated. -.-

--- c_src/utils/exceptions.h
+++ c_src/utils/exceptions.h
@@ -1,4 +1,16 @@
+/**
+ * @doc Custom exception classes.
+ *
+ * @copyright 2012-2013 David H. Bronke and Christopher S. Case
+ * Licensed under the MIT license; see the LICENSE file for details.
+ */
+
+#ifndef __EXCEPTIONS__
+#define __EXCEPTIONS__
+
+
 #include <exception>
+#include <string>
 
 
 template<typename T>
@@ -11,3 +23,21 @@ public:
 private:
 	T index;
 }; // end BadIndex
+
+
+// Implementation (can't be in a separate file; see http://stackoverflow.com/a/11641429)
+
+template<typename T>
+BadIndex<T>::BadIndex(T index) :
+	index(index)
+{
+} // end constructor
+
+template<typename T>
+const char* BadIndex<T>::what() const throw()
+{
+	return "Bad index";
+} // end what
+
+
+#endif // __EXCEPTIONS__
