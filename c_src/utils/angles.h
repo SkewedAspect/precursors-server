8e6b64d9c9461ed846dbf4baca9a065675d452fe
David H. Bronke <whitelynx@gmail.com>
2013-04-06 20:13:11 -0500

    Moved deg2rad and rad2deg into their own file.

---
+++ c_src/utils/angles.h
@@ -0,0 +1,16 @@
+/**
+ * @doc Helper functions for working with angles.
+ *
+ * @copyright 2012-2013 David H. Bronke and Christopher S. Case
+ * Licensed under the MIT license; see the LICENSE file for details.
+ */
+
+#ifndef __ANGLES__
+#define __ANGLES__
+
+
+double rad2deg(double radians);
+double deg2rad(double degrees);
+
+
+#endif // __ANGLES__
