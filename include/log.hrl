7ab5f164600876f653d70eef101d0e9fa6698f71
David H. Bronke <whitelynx@gmail.com>
2013-04-30 20:04:04 -0500

    Fixed change from 1b20cbc34ca7; added 'warn' alias to the TEST branch in log.hrl.

--- include/log.hrl
+++ include/log.hrl
@@ -23,6 +23,9 @@
 -define(notice(Message), ?debugFmt("[~p][~p][~p]~n	NOTICE: ~s~n", [erlang:localtime(), node(), self(), Message])).
 -define(notice(Message, Args), ?debugFmt("[~p][~p][~p]~n	NOTICE: ~s~n", [erlang:localtime(), node(), self(), lists:flatten(io_lib:format(Message, Args))])).
 
+-define(warn(Message), ?debugFmt("[~p][~p][~p]~n	WARNING: ~s~n", [erlang:localtime(), node(), self(), Message])).
+-define(warn(Message, Args), ?debugFmt("[~p][~p][~p]~n	WARNING: ~s~n", [erlang:localtime(), node(), self(), lists:flatten(io_lib:format(Message, Args))])).
+
 -define(warning(Message), ?debugFmt("[~p][~p][~p]~n	WARNING: ~s~n", [erlang:localtime(), node(), self(), Message])).
 -define(warning(Message, Args), ?debugFmt("[~p][~p][~p]~n	WARNING: ~s~n", [erlang:localtime(), node(), self(), lists:flatten(io_lib:format(Message, Args))])).
 
