55236e3d1a4b3996018967de73edc5bcc6dec69e
David H. Bronke <whitelynx@gmail.com>
2013-09-24 21:22:34 -0500

    Removed riak_auth_app.erl, instead supervising the riak_auth process inside the main app.

--- src/pre_server_sup.erl
+++ src/pre_server_sup.erl
@@ -48,10 +48,6 @@ init(Args) ->
 	AuthManagerArgs = proplists:get_value(auth_backends, Args, []),
 	AuthManagerKid = ?CHILD(pre_gen_auth, worker, [AuthManagerArgs]),
 
-	% Load any plugins
-	PluginsToLoad = proplists:get_value(plugins, Args, []),
-	timer:apply_after(10, pre_util, start_apps, [PluginsToLoad]),
-
 	Kids = [
 		AuthManagerKid,
 		ListenerKid,
