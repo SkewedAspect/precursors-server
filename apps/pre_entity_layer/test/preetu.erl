125a5aff8557ce58a2973a5cdc72f332b2c7746e
Micah Warren <micahw@lordnull.com>
2014-02-14 17:08:14 -0600

    Added kill for the ol' kill 'n' wait.

--- apps/pre_entity_layer/test/preetu.erl
+++ apps/pre_entity_layer/test/preetu.erl
@@ -2,6 +2,17 @@
 
 -compile(export_all).
 
+kill(undefined) ->
+	ok;
+
+kill(Atom) when is_atom(Atom) ->
+	kill(whereis(Atom));
+
+kill(Pid) when is_pid(Pid) ->
+	unlink(Pid),
+	exit(Pid, kill),
+	wait_for_exit(Pid).
+
 wait_for_exit(undefined) ->
 	ok;
 
